b423a20918e41021514bad13c613f143d8a8479f
albert-Pan <1251630240@qq.com>
2019-04-14 00:03:39 +0800

    spring boot demo，集成jap fastjson 和druid数据源

---
+++ demo1/src/main/java/com/pw/example/demo1/entity/UserEntity.java
@@ -0,0 +1,46 @@
+package com.pw.example.demo1.entity;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+public class UserEntity  implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
---
+++ demo1/src/main/java/com/pw/example/demo1/jap/UserJPA.java
@@ -0,0 +1,14 @@
+package com.pw.example.demo1.jap;
+
+import com.pw.example.demo1.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.io.Serializable;
+
+public interface UserJPA extends
+        JpaRepository<UserEntity, Long>,
+        JpaSpecificationExecutor<UserEntity>,
+        Serializable {
+
+}
